2cf8b6d433840180ee2a346322ca6ed8a88cf6e7
trungls <125449479+trungls@users.noreply.github.com>
2023-06-21 01:15:50 +0700

    Update products.js

--- public/js/products.js
+++ public/js/products.js
@@ -1,67 +1,71 @@
-// Render card sản phẩm
-function renderProductCard(product) {
-  const card = document.createElement('div');
-  card.classList.add('card', 'col-md-4', 'col-lg-3', 'mb-4');
+// Lấy dữ liệu từ file data.json
+fetch('data.json')
+  .then(response => response.json())
+  .then(data => {
+    // Tạo card cho mỗi sản phẩm
+    const container = document.createElement('div');
+    container.className = 'container';
 
-  const image = document.createElement('img');
-  image.src = product.image;
-  image.alt = 'Product Image';
-  image.classList.add('card-img-top');
+    const row = document.createElement('div');
+    row.className = 'row';
+
+    data.product.forEach(product => {
+      const col = createProductCard(product);
+      row.appendChild(col);
+    });
+
+    container.appendChild(row);
+    document.getElementById('product-card').appendChild(container);
+  })
+  .catch(error => {
+    console.error('Lỗi khi tải dữ liệu:', error);
+  });
+
+// Hàm tạo card cho một sản phẩm
+function createProductCard(product) {
+  const { id, name, description, image, price, salePrice } = product;
+
+  // Tạo các phần tử HTML cho card sản phẩm
+  const col = document.createElement('div');
+  col.className = 'col-lg-4 col-md-6 col-xl-3';
+
+  const card = document.createElement('div');
+  card.className = 'card';
+
+  const imageElement = document.createElement('img');
+  imageElement.src = image;
+  imageElement.className = 'card-img-top';
+  imageElement.alt = name;
 
   const cardBody = document.createElement('div');
-  cardBody.classList.add('card-body');
+  cardBody.className = 'card-body';
 
-  const title = document.createElement('h5');
-  title.classList.add('card-title', 'text-left', 'mt-1');
-  title.textContent = product.name;
+  const productName = document.createElement('h5');
+  productName.className = 'card-title';
+  productName.textContent = name;
 
-  const price = document.createElement('div');
-  price.classList.add('card-price', 'd-inline', 'h5', 'mt-1');
-  price.textContent = `$${product.price}`;
+  const productDescription = document.createElement('p');
+  productDescription.className = 'card-text';
+  productDescription.textContent = description;
 
-  const salePrice = document.createElement('div');
-  salePrice.classList.add('card-price-sale', 'd-inline', 'p-2', 'h6', 'text-danger', 'text-decoration-line-through');
-  salePrice.textContent = `$${product.salePrice}`;
+  const priceElement = document.createElement('p');
+  priceElement.className = 'card-text';
+  priceElement.textContent = 'Price: ' + price;
 
-  const btnContainer = document.createElement('div');
-  btnContainer.classList.add('card-btn', 'mt-1', 'text-center');
+  const salePriceElement = document.createElement('p');
+  salePriceElement.className = 'card-text';
+  salePriceElement.textContent = 'Sale Price: ' + salePrice;
 
-  const btn = document.createElement('a');
-  btn.href = '#';
-  btn.classList.add('btn', 'btn-primary');
-  btn.textContent = 'Thêm vào giỏ hàng';
+  // Gắn các phần tử con vào card
+  cardBody.appendChild(productName);
+  cardBody.appendChild(productDescription);
+  cardBody.appendChild(priceElement);
+  cardBody.appendChild(salePriceElement);
 
-  btnContainer.appendChild(btn);
-
-  cardBody.appendChild(title);
-  cardBody.appendChild(price);
-  cardBody.appendChild(salePrice);
-  cardBody.appendChild(btnContainer);
-
-  card.appendChild(image);
+  card.appendChild(imageElement);
   card.appendChild(cardBody);
 
-  return card;
+  col.appendChild(card);
+
+  return col;
 }
-
-// Render tất cả các card sản phẩm từ data.json
-function renderAllProductCards() {
-  const productCardContainer = document.getElementById('product-card');
-
-  // Fetch dữ liệu từ tệp JSON
-  fetch('data.json')
-    .then(response => response.json())
-    .then(data => {
-      // Lặp qua tất cả các sản phẩm và hiển thị card sản phẩm
-      data.forEach(product => {
-        const card = renderProductCard(product);
-        productCardContainer.appendChild(card);
-      });
-    })
-    .catch(error => {
-      console.error('Lỗi khi tải dữ liệu:', error);
-    });
-}
-
-// Gọi hàm để render tất cả các card sản phẩm
-renderAllProductCards();
